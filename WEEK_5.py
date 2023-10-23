fd6c8b670ee9b4fd35d46d8e408577100553e527
ayeshatajarab <A.tajarab6656@student.leedsbeckett.ac.uk>
2023-10-23 17:01:08 +0100

    the first commit

---
+++ WEEK_5.py
@@ -0,0 +1,10 @@
+#Task 1 - Executing a file
+number = input ("Enter a number:")
+number = int(number)
+print("The number entered was", number)
+if (number %2) == 0:
+    print("That is an even number")
+else:
+    print("That is an odd number")
+
+#Task 2 - Adding comments to each statement
