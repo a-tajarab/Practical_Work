33939acb426677c63386484ff399c2e457bf2437
c3667332 <A.Tajarab6656@student.leedsbeckett.ac.uk>
2023-10-24 14:13:06 +0100

    updated

--- WEEK_5.py
+++ WEEK_5.py
@@ -8,4 +8,16 @@ if (number %2) == 0:
 else:
     print("That is an odd number")
 
-#Task 2 - Adding comments to each statement
+#Task 2 - Adding comments to each statement# python total.py 25 90 15
+import sys
+count = len(sys.argv)
+total = number + 20
+while count > 1:
+    count -= 1
+    total += float(sys.argv[count])
+print("Total is", total)
+
+response = ""
+while response != 'x' and response != 'z':
+     response = input("x or z")
+print("you responded with",response)
