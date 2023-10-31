ddbfe7bff2cdc283ab13c11e516909892ac9ba6a
c3667332 <A.Tajarab6656@student.leedsbeckett.ac.uk>
2023-10-31 12:59:30 +0000

    updated 31/10/2023

--- WEEK_5.py
+++ WEEK_5.py
@@ -21,3 +21,20 @@ response = ""
 while response != 'x' and response != 'z':
      response = input("x or z")
 print("you responded with",response)
+
+#Task 3 - my_utils.py
+def average(values):
+    """ Calculates the average of the given list. """
+    total = 0;
+    for n in values:                          #total the given values
+        total += float(n)
+    return total/len(values)                  #return calculated average
+#initialisation statement
+print("Welcome, utils module has been imported and initialised!")
+
+
+#Task 4 - Module Search Path
+import sys
+print ("The import search path for this program is", sys.path)
+
+
