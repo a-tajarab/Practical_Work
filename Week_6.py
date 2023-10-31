b0c34c34938c986657618f76467ce425e2773e3a
c3667332 <A.Tajarab6656@student.leedsbeckett.ac.uk>
2023-10-31 13:36:20 +0000

    updated 31/10/2023

--- Week_6.py
+++
@@ -1,9 +0,0 @@
-#Task 1
-import math
-
-if __name__ == '__main__':
- squares = [1, 4, 9, 16, 25]
-
-for num in squares:
-    square_root = math.sqrt(num)
-    print("The square root of", {num}, " is", {square_root})
