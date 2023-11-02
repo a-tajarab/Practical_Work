9f6b2309b447584e8bdbbc85b9e2c141a6e0b990
ayeshatajarab <A.tajarab6656@student.leedsbeckett.ac.uk>
2023-11-02 14:52:34 +0000

    updated 2_11_2023

--- WEEK_5.py
+++ WEEK_5.py
@@ -9,3 +9,22 @@ else:
     print("That is an odd number")
 
 #Task 2 - Adding comments to each statement
+
+
+#Task 3 - Independent execution
+number_2 = input ("Enter a number:")
+number_2 = int(number_2)
+print("The number you entered was", number_2)
+if (number_2 %10) == 0:
+    print("That number is divisible by 10")
+else:
+    print("That number is not divisible by 10")
+
+#Task 4 - total.py
+import sys
+count = len(sys.argv)
+total = 0
+while count > 1:
+    count -= 1
+    total += 1
+print("Total is", total)
