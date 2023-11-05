0d2bccd3f94c2be299d37dddbc683875f3e0d834
ayeshatajarab <A.tajarab6656@student.leedsbeckett.ac.uk>
2023-11-05 13:22:47 +0000

    updated_3 5_11_2023

--- WEEK_5.py
+++ WEEK_5.py
@@ -28,13 +28,36 @@ while count > 1:
     count -= 1
     total += 1
 print("Total is", total)
-number_3 =input ("Type a number of your choice:")
+number_3 = input("Enter in your age:")
 number_3 = int(number_3)
 print("Wow what a lovely number", number_3, "love it")
 print("Right! Let's get onto some fun stuff")
+print("Disclaimer there is insensitive words")
 print("Would you like to proceed?")
 response = input("Y or N:")
 if response == "Y":
     print("Fabulous!")
+    if number_3 < 0 <= 10:
+        print("You are boss!")
+    if number_3 >= 10:
+        print("You will become a star!")
+    if number_3 >= 20:
+        print("Life will get easier. Hang on.")
+    if number_3 >= 30:
+        print("You feel old now!")
+    if number_3 >= 40:
+        print("Happy days")
+    if number_3 >= 50:
+        print("Half-life hahaha")
+    if number_3 >= 60:
+        print("Are you depressed?")
+    if number_3 >= 70:
+        print("You seem wise!")
+    if number_3 >= 80:
+        print("You have a bright future ahead of you")
+    if number_3 >= 90:
+        print("Stay healthy!")
 elif response == "N":
     print("Oh well I guess you will miss the fun!:(")
+
+
