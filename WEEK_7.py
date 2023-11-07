12f580e402c0178c077479d0f6fd9e342ee29a4e
c3667332 <A.Tajarab6656@student.leedsbeckett.ac.uk>
2023-11-07 12:22:16 +0000

    update_2  7_11_2023

--- WEEK_7.py
+++ WEEK_7.py
@@ -2,3 +2,9 @@
 
 names = {"John", "Eric", "Terry", "Micheal", "Graham", "Terry"}
 print(names)
+
+#Task 2 - Set comprehensions
+sentence = "this is a sentence containing some letters"
+unique_letters = {x for x in sentence}
+print(sentence)
+
